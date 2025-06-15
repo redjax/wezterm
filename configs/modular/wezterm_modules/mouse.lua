8f0829bba1d6bcbf2d4936d4ae4c5da57295ad17
redjax <5534031+redjax@users.noreply.github.com>
2025-06-15 15:20:07 -0400

    Modify right click behavior.
    Paste when right clicking, copy if text is highlighted

--- configs/modular/wezterm_modules/mouse.lua
+++ configs/modular/wezterm_modules/mouse.lua
@@ -1,12 +1,27 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
 return function(config)
-    -- Set mouse bindings
-    config.mouse_bindings = {
-      {
-        -- CTRL+LeftClick = open link
-        event={Up={streak=1, button="Left"}},
-        mods="CTRL",
-        action="OpenLinkAtMouseCursor",
-      },
-    }
+  config.mouse_bindings = {
+    {
+      -- CTRL+LeftClick = open link
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "CTRL",
+      action = act.OpenLinkAtMouseCursor,
+    },
+    {
+      -- RightClick: Copy if selection, else paste
+      event = { Down = { streak = 1, button = "Right" } },
+      mods = "NONE",
+      action = wezterm.action_callback(function(window, pane)
+        local sel = window:get_selection_text_for_pane(pane)
+        if sel ~= "" then
+          window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+          window:perform_action(act.ClearSelection, pane)
+        else
+          window:perform_action(act.PasteFrom("Clipboard"), pane)
+        end
+      end),
+    },
+  }
 end
-  
