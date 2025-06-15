43d01a09390cfe7343b0ccb1e5fe2dd8ca1e42f0
redjax <5534031+redjax@users.noreply.github.com>
2025-06-15 15:20:07 -0400

    Fix wezterm install script

--- scripts/windows/install-wezterm-config.ps1
+++ scripts/windows/install-wezterm-config.ps1
@@ -16,10 +16,25 @@ $CWD = $PWD.Path
 $WEZTERM_CONFIG_SRC = ( Join-Path -Path $CWD -ChildPath ( Join-Path -Path "configs" -ChildPath $ConfigName ) )
 Write-Debug "Wezterm config source: $WEZTERM_CONFIG_SRC"
 
+## Path to ~/.config
+$HOST_DOTCONFIG_DIR = "$env:USERPROFILE\.config"
+
 ## Check if config sourcce exists
 if ( -not ( Test-Path $WEZTERM_CONFIG_SRC ) ) {
     Write-Error "Wezterm source config directory not found: $WEZTERM_CONFIG_SRC"
     exit 1
+}
+
+## Check if ~/.config exists, create if not
+if ( -not ( Test-Path $HOST_DOTCONFIG_DIR -PathType Container -ErrorAction SilentlyContinue ) ) {
+    Write-Warning "~/.config directory not found. Creating"
+    try {
+        New-Item -Path $HOST_DOTCONFIG_DIR -ItemType Directory
+    }
+    catch {
+        Write-Error "Error creating ~/.config directory. Details: $($_.Exception.Message)"
+        exit 1
+    }
 }
 
 function Test-IsAdmin {
@@ -38,7 +53,7 @@ function Start-AsAdmin {
     $isAdmin = [bool](New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
     
     if (-not $isAdmin) {
-        # Prompt to run as administrator if not already running as admin
+        ## Prompt to run as administrator if not already running as admin
         $arguments = "-Command `"& {$command}`""
         Write-Debug "Running command: Start-Process powershell -ArgumentList $($arguments) -Verb RunAs"
 
@@ -51,15 +66,17 @@ function Start-AsAdmin {
         }
     }
     else {
-        # If already running as admin, execute the command
+        ## If already running as admin, execute the command
         Invoke-Expression $command
-        return $false  # Indicate that the command was run without elevation
+
+        ## Indicate that the command was run without elevation
+        return $false
     }
 }
 
-## Check if ~/.wezterm.lua already exists, backup if so
-if ( Test-Path -Path "$env:USERPROFILE\.wezterm.lua" ) {
-    $Item = Get-Item "$env:USERPROFILE\.wezterm.lua"
+## Check if ~/.wezterm already exists, backup if so
+if ( Test-Path -Path "$env:USERPROFILE\.wezterm" ) {
+    $Item = Get-Item "$env:USERPROFILE\.wezterm"
 
     ## Check if path is a junction
     if ( $Item.Attributes -band [System.IO.FileAttributes]::ReparsePoint ) {
@@ -85,17 +102,21 @@ if ( Test-Path -Path "$env:USERPROFILE\.wezterm.lua" ) {
 
 function New-Symlink {
     Param(
-        [Parameter(Mandatory = $false, HelpMessage = "Composed symlink command to run")]
-        [string]$SymlinkCommand
+        [Parameter(Mandatory = $true, HelpMessage = "Source file")]
+        [string]$SRC,
+        [Parameter(Mandatory = $true, HelpMessage = "Destination file")]
+        [string]$DEST
     )
     $isAdmin = Test-IsAdmin
+
+    $SymlinkCommand = "New-Item -Path $DEST -ItemType SymbolicLink -Value $SRC"
 
     if ( $isAdmin ) {
         try {
             Invoke-Expression $SymlinkCommand
         }
         catch {
-            Write-Error "Error creating symlink from $SRC_FILE to $DEST_FILE. Details: $($_.Exception.Message)"
+            Write-Error "Error creating symlink from $SRC to $DEST. Details: $($_.Exception.Message)"
             exit 1
         }
     }
@@ -105,88 +126,19 @@ function New-Symlink {
             Start-AsAdmin $SymlinkCommand
         }
         catch {
-            Write-Error "Error creating symlink from $SRC_FILE to $DEST_FILE. Details: $($_.Exception.Message)"
+            Write-Error "Error creating symlink from $SRC to $DEST. Details: $($_.Exception.Message)"
             exit 1
         }
     }
-}
-
-function New-FileSymlink {
-    Param(
-        [Parameter(Mandatory = $true, HelpMessage = "Source file")]
-        [string]$SRC_FILE,
-        [Parameter(Mandatory = $true, HelpMessage = "Destination file")]
-        [string]$DEST_FILE
-    )
-
-    Write-Host "Creating symlink from $SRC_FILE to $DEST_FILE" -ForegroundColor Green
-
-    $SymlinkCommand = "New-Item -Path $DEST_FILE -ItemType SymbolicLink -Value $SRC_FILE"
-    New-SymLink -SymlinkCommand $SymlinkCommand
-}
-
-function New-DirSymlink {
-    Param(
-        [Parameter(Mandatory = $true, HelpMessage = "Source directory")]
-        [string]$SRC_DIR,
-        [Parameter(Mandatory = $true, HelpMessage = "Destination directory")]
-        [string]$DEST_DIR
-    )
-
-    Write-Host "Creating symlink from $SRC_DIR to $DEST_DIR" -ForegroundColor Green
-
-    $SymlinkCommand = "New-Item -Path $DEST_DIR -ItemType SymbolicLink -Value $SRC_DIR"
-    New-SymLink -SymlinkCommand $SymlinkCommand
 }
 
 ## Symlink ~/.wezterm.lua
 try {
-    New-FileSymlink -SRC_FILE "$WEZTERM_CONFIG_SRC\.wezterm.lua" -DEST_FILE "$env:USERPROFILE\.wezterm.lua"
+    $SymlinkSuccess = New-Symlink -SRC "$WEZTERM_CONFIG_SRC" -DEST "$HOST_DOTCONFIG_DIR\wezterm"
 }
 catch {
     Write-Error "Error creating symlink for Wezterm Lua config file. Details: $($_.Exception.Message)"
     exit 1
-}
-
-## Check if wezterm_modules dir exists in src
-if ( Test-Path -Path ( Join-Path -Path $WEZTERM_CONFIG_SRC -ChildPath "wezterm_modules" -ErrorAction SilentlyContinue ) ) {
-    Write-Host "Discovered a wezterm_modules directory in source config." -ForegroundColor Cyan
-
-    if ( Test-Path "$env:USERPROFILE\.wezterm_modules" ) {
-
-        ## Check if ~/.wezterm already exists, backup if so
-        $Item = Get-Item "$env:USERPROFILE\.wezterm"
-
-        ## Check if path is a junction
-        if ( $Item.Attributes -band [System.IO.FileAttributes]::ReparsePoint ) {
-            Write-Host "Path is already a junction: $Item" -ForegroundColor Green
-        }
-        else {
-            ## Path is a regular directory
-            Write-Warning "Path already exists: $Item. Moving to $($Item).bak"
-            If ( Test-Path "$($Item).bak" ) { 
-                Write-Warning "$($Item).bak already exists. Overwriting backup."
-                Remove-Item -Recurse "$($Item).bak"
-            }
-
-            try {
-                Move-Item $Item "$($Item).bak"
-            }
-            catch {
-                Write-Error "Error moving $Item to $($Item).bak. Details: $($_.Exception.Message)"
-                exit 1
-            }
-        }
-
-        ## Symlink ~/.wezterm
-        try {
-            New-DirSymlink -SRC_DIR "$WEZTERM_CONFIG_SRC\wezterm_modules" -DEST_DIR "$env:USERPROFILE\.wezterm"
-        }
-        catch {
-            Write-Error "Error creating symlink for Wezterm modules directory. Details: $($_.Exception.Message)"
-            exit 1
-        }
-    }
 }
 
 Write-Host "Finished installing Wezterm config." -ForegroundColor Green
